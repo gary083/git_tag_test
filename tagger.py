38ff3150c5bc3c165edeca0a6e23a3061be101e9
Gary Chen <guanyuchen@google.com>
2023-03-07 08:34:00 +0000

    change something

--- tagger.py
+++ tagger.py
@@ -3,9 +3,11 @@ import logging
 import subprocess
 from typing import List, Union
 
+# 3.1.30
 import git
 
 repo = git.Repo(subprocess.getoutput('git rev-parse --show-toplevel'))
+
 
 def get_tags(repo) -> List[str]:
     """Get list of all tags for package"""
@@ -15,6 +17,7 @@ def get_tags(repo) -> List[str]:
         # In case a git repo is not found
         return []
 
+
 def get_head_tag(repo):
     HeadTag = None
     for tagRef in repo.tags:
@@ -22,11 +25,51 @@ def get_head_tag(repo):
             HeadTag = tagRef.tag.tag
     return HeadTag
 
+
+def check_head(repo):
+    return any([tag.commit == repo.head.commit for tag in repo.tags])
+
+
+print(check_head(repo))
+
+
 def get_latest_tag(repo):
     return max(get_tags(repo))
 
-print (get_latest_tag(repo))
 
-# hotfix = False
-# def get_next_tag(repo, major, hotfix):
+def parse_tag(tag):
+    return map(int, tag.split('.'))
 
+
+print(get_head_tag(repo))
+print(get_latest_tag(repo))
+
+
+def get_next_tag(repo, new_major=None, hotfix=False):
+    latest_tag = get_latest_tag(repo)
+    major, minor, patch = parse_tag(latest_tag)
+    if new_major:
+        # Need to incremental new_major > major
+        return f"{new_major}.{0}.{0}"
+    if hotfix:
+        return f"{major}.{minor}.{patch+1}"
+    return f"{major}.{minor+1}.0"
+
+
+print(get_next_tag(repo, new_major=2))
+print(get_next_tag(repo, hotfix=True))
+print(get_next_tag(repo))
+
+next_tag = get_next_tag(repo)
+
+
+def add_tag(repo, tag):
+    new_tag = repo.create_tag(tag, message='Automatic tag "{0}"'.format(tag))
+    return new_tag
+
+
+def push_tag(repo, tag):
+    repo.remote('origin').push(tag)
+
+new_tag = add_tag(repo, next_tag)
+push_tag(repo, new_tag)
